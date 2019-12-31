c3d6a8cfe516c57593a401059339ac627a540bf4
John Doe <johndoe@example.com>
2019-12-31 10:57:46 +0700

    baru banget2

--- app/src/main/java/com/example/mycollegeapp/MainActivity.java
+++ app/src/main/java/com/example/mycollegeapp/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.mycollegeapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mycollegeapp.Activity.Listrik.MenuListrik;
 import com.example.mycollegeapp.Activity.PDAM.InputPdam;
 import com.example.mycollegeapp.Activity.Pulsa.ChooseProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_logout;
+    TextView txt_id, txt_username;
+    String id, username;
+    SharedPreferences sharedpreferences;
+
+    public static final String TAG_ID = "id";
+    public static final String TAG_USERNAME = "username";
 
     ImageView llPulsa;
     ImageView llListrik;
@@ -20,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt_id = (TextView) findViewById(R.id.user_name);
+        txt_username = (TextView) findViewById(R.id.saldo);
+
+        sharedpreferences = getSharedPreferences(Login.my_shared_preferences, Context.MODE_PRIVATE);
+
+        id = getIntent().getStringExtra(TAG_ID);
+        username = getIntent().getStringExtra(TAG_USERNAME);
+
+        txt_id.setText("ID : " + id);
+        txt_username.setText("USERNAME : " + username);
 
         initialaze();
         onClick();
